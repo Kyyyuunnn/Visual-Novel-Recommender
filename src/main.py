2bfc774920920dfea90f4f5a323a30cad85adf22
Steven Nguyen <stevennguyen4200@gmail.com>
2025-06-19 14:23:06 -0500

    implementing fetching user lists

--- src/main.py
+++ src/main.py
@@ -1,25 +1,26 @@
-from login import login, auto_login, logout
-from manual_search import manual_search, vn_search
 import os
 import asyncio
+from login import login, auto_login, logout
+from manual_search import manual_search, vn_search
+from fetch_user_list import fetch_user_list, read_saved_user_info
 
 def main():
     info_file = "info.txt"
 
+    token = None
+    username = None
+
     if os.path.exists(info_file):
         with open(info_file, "r") as f:
             lines = f.readlines()
-            token = None
-            username = None
             for line in lines:
                 if line.startswith("token="):
-                    token = line.strip().split("=")[1]
+                    token = line.strip().split("=", 1)[1]
                 elif line.startswith("username="):
-                    username = line.strip().split("=")[1]
-        if username:
-            print(f"Welcome Back {username}! ")
-        else: 
-            print(f"Welcome Back! ")
+                    username = line.strip().split("=", 1)[1]
+
+    if username:
+        print(f"Welcome Back {username}! ")
     else:
         print(f"Welcome to VNReccommender! ")
     
@@ -27,27 +28,38 @@ def main():
 
     print("1: Login (please go through this option at least once before beginning to use the application.) ")
     print("2: Manually input novels")
-    print("3: Import from user list")
-    print("4: Logout")
+    print("3: Import your VNDB read list")
+    print("4: Import another user's list")
+    print("5: Logout")
 
-    choice = input("Enter your choice (1-4): ")
+    choice = input("Enter your choice (1-5): ").strip()
 
     # placeholder functions
     if choice == "1":
         token = input("Please enter your VNDB API token: ").strip()
         login(token)
+        username, token = read_saved_user_info()
     elif choice == "2": 
-        asyncio.run(manual_search(token))
+        if token:
+            asyncio.run(manual_search(token))
+        else:
+            print("You must login first.")
     elif choice == "3":
-        break
+        if username and token:
+            fetch_user_list(username, token)
+        else:
+            print("You must login first before importing your list.")
     elif choice == "4":
+        if token:
+            other_user_id = input("Input the user's ID you'd like to import (e.g. u123456): ").strip()
+            fetch_user_list(other_user_id, token)
+        else:
+            print("You must login first before importing another user's list.")
+    elif choice == "5":
         logout()
-        #print("Goodbye! Enjoy reading! ")
     else:
         print("Invalid option ")
-
 
 # for main to exe
 if __name__ == "__main__":
     main()
-    
