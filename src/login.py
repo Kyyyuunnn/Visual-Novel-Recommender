de7a232bd830a58de87c19c8b33727405263a0fa
Steven Nguyen <stevennguyen4200@gmail.com>
2025-06-19 14:21:23 -0500

    login now include u#

--- src/login.py
+++ src/login.py
@@ -9,10 +9,11 @@ async def login_and_greet(token: str):
         async with Client(token=token) as client:
             auth_info = await client.get_auth_info()
             username = auth_info.username
-            
+            user_id = auth_info.id  # grabs id
+
             # saves token
             with open(info_file, "w") as f:
-                f.write(f"token={token}\nusername={username}")
+                f.write(f"token={token}\nusername={username}\nuser_id={user_id}")
 
             print(f"Login successful! Welcome {username}!")
 
@@ -22,19 +23,23 @@ async def login_and_greet(token: str):
 def login(token: str):
     asyncio.run(login_and_greet(token))
 
-
 def auto_login():
     if os.path.exists(info_file):
-        with open(token_file, "r") as f:
-            saved_token = f.read().strip()
-        login(saved_token)
-        return True
+        try:
+            with open(info_file, "r") as f:
+                lines = f.read().splitlines()
+                token = next(line.split("=")[1] for line in lines if line.startswith("token="))
+            login(token)
+            return True
+        except Exception as e:
+            print("Auto login failed:", e)
+            return False
     else: 
         return False
 
 def logout():
     if os.path.exists(info_file):
-        os.remove(token_file)
+        os.remove(info_file)
         print("Logged out successfully.")
     else:
         print("You are not logged in.")
